d92a08709b2e839011ec45871046890a18d9cef6
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-03-09 07:07:42 +0100

    done

--- option2.rb
+++ option2.rb
@@ -9,9 +9,7 @@ smallest_numbers = {
 	six: {number: 6, name: "six" },
 	seven: {number: 7, name: "seven" },
 	eigth: {number: 8, name: "eigth" },
-	nine: {number: 9, name: "nine" }
-}
-smallest_numbers_max = {
+	nine: {number: 9, name: "nine" },
 	ten: {number: 10, name: "ten" },
 	eleven: {number: 11, name: "eleven" },
 	twelve: {number: 12, name: "twelve" },
@@ -47,26 +45,79 @@ join_and = "and"
 
 class Option2
 	attr_accessor :number_storage, :current_number,
-				  :smallest_numbers, :smallest_numbers_max, :small_numbers, :large_numbers
+				  :smallest_numbers, :small_numbers, :large_numbers
 
 	def get_number_name
-	  array_of_numbers = put_numbers_into_array
-	  number_name = ""
-	  if smallest_number?
-	  	number_name = get_name_from_smallest_number(array_of_numbers, smallest_numbers)
-	  elsif smallest_numbers_max?
-	  	number_name = get_name_from_smallest_number_max(array_of_numbers, smallest_numbers_max)
-	  elsif small_number?
-	  	number_name = give_a_name_to_numbers(array_of_numbers, small_numbers, smallest_numbers)
-	  else
-	  	number_name = give_a_name_to_numbers(array_of_numbers, large_numbers)
-	  end
-	  return number_name
 
+		number_name = ""
+
+		if smallest_number?
+			number_name = get_name(@current_number, smallest_numbers)
+		elsif small_number?
+			number_name = get_name(@current_number, small_numbers)
+			if number_name.nil?
+				array_of_numbers = convert_number_to_array(@current_number)
+				number_name = get_name_of_two_digits(array_of_numbers)
+			end
+		elsif larg_number_hundred?
+			number_name = get_name(@current_number, large_numbers)
+			if number_name.nil?
+				number1 = @current_number/100
+				number2 = @current_number%100
+				if number2 >19
+				  number_name = "#{get_name(number1, smallest_numbers)} hundred and #{get_name(number2, small_numbers)}"
+				else
+				  number_name = "#{get_name(number1, smallest_numbers)} hundred and #{get_name(number2, smallest_numbers)}"
+			 	end
+			end
+		else
+			number_name = get_name(@current_number, large_numbers)
+			if number_name.nil?
+				array_of_numbers = convert_number_to_array(@current_number)
+				number1 = get_first_number_from_array(array_of_numbers)
+			    array_of_numbers.shift
+			    number_name = "#{get_name(number1, smallest_numbers)} thousand, #{get_name_of_three_digits(array_of_numbers)}"
+	 
+			end
+		end
+
+		return number_name 
+	end
+	def get_name_of_two_digits(array_of_numbers)
+  	  number1 = get_first_number_and_add_zero_from_array(array_of_numbers)
+	  number2 = get_last_number_from_array(array_of_numbers)
+	  number_name = "#{get_name(number1, small_numbers)} #{get_name(number2, smallest_numbers)}"
+	  return number_name
 	end
 
-		def put_numbers_into_array
-			
+	def get_name_of_three_digits(array_of_numbers)
+	  number1 = get_first_number_from_array(array_of_numbers)
+	  array_of_numbers.shift
+	  number2 = (array_of_numbers.first.to_s+""+array_of_numbers.last.to_s).to_i
+	  @current_number = number2
+
+	  if smallest_number?
+	  	number_name = "#{get_name(number1, smallest_numbers)} hundred and #{get_name(number2, smallest_numbers)}"
+	  else
+	  	number_name = "#{get_name(number1, smallest_numbers)} hundred and #{get_name(number2, small_numbers)}"
+	  end
+	  return number_name
+	end
+	def get_first_number_and_add_zero_from_array(array_of_numbers)
+	  (array_of_numbers.first.to_s+"0").to_i
+	end
+	def get_middle_number_and_add_zero_from_array(array_of_numbers)
+		array_of_numbers.shift
+		return (array_of_numbers.first.to_s+"0").to_i
+	end
+	def get_first_number_from_array(array_of_numbers)
+	  (array_of_numbers.first.to_s).to_i
+	end
+
+	def get_last_number_from_array(array_of_numbers)
+	  (array_of_numbers.last.to_s).to_i
+	end
+	def convert_number_to_array(number)
 			@number_storage = []
 
 			while @current_number > 0
@@ -76,74 +127,38 @@ class Option2
 				
 			end
 			return @number_storage.reverse
-		end
-
-		def give_a_name_to_numbers(array_of_numbers, hash_numbers)
-
-		end
+	end
 
 
-		def get_name_from_smallest_number(array_of_numbers, smallest_numbers)
-			
-			puts @current_number
-			smallest_numbers.each do |key,value| 
-			 number_position = value[:number]
 
-				 if value[:number] == @current_number
-				  number_name += key.to_s
-				  break
+	def get_name(current_number, hash_numbers)
+
+	  hash_numbers.each do |key, value|
+				if value[:number] == current_number
+					return value[:name]
 				end
-			end
-			return number_name
-		end
+	  end
+	  return nil
+	end
 
-		def get_name_from_smallest_number_max(array_of_numbers, smallest_numbers_max)
-			
-			number_name = ""
-			smallest_numbers_max.each do |key,value| 
-			 number_position = value[:number]
+	def smallest_number?
+	  ( (@current_number >= 0) and (@current_number<=19) )
+	end
+	def small_number?
+		((@current_number >= 20) and (@current_number <= 99))
+	end
 
-				 if value[:number] == @current_number
-				  number_name += key.to_s
-				  break
-				end
-			end
-			return number_name
-		end
-
-		def get_name_from_small_number(array_of_numbers, small_numbers)
-			puts small_numbers
-			number_name = ""
-			puts "complete number = #{@current_number}"
-
-			small_numbers.each do |key,value| 
-			 number_position = value[:number]
-
-				 if value[:number] == @current_number
-				   puts "#{key}"
-				  break
-				end
-			end
-
-			return number_name
-		end
-
-
-		
-		def smallest_number?
-			@current_number <= 9
-		end
-		def smallest_numbers_max?
-			@current_number <= 19
-		end
-
+	def larg_number_hundred?
+		((@current_number >= 100) and (@current_number <= 999))
+	end
 end
 
 
-
+#317
 n = Option2.new
-n.current_number = 13
+n.current_number = 1317
 n.smallest_numbers = smallest_numbers
 n.small_numbers = small_numbers
+n.large_numbers = large_numbers
 puts n.get_number_name
 
