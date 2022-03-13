fb217e65b153132a2733f1581a6ef9280169ac5d
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-03-13 03:26:00 +0100

    numbers tested

--- spec/option2_spec.rb
+++ spec/option2_spec.rb
@@ -3,39 +3,58 @@ RSpec.describe "Option2" do
 
 let(:n) { Option2.new}
 
-    it "returns zero" do 
-      n.current_number = 0 
-      expect(n.get_number_name).to eq("zero")
+    describe "know numbers" do
+        it "returns zero" do 
+        n.current_number = 0 
+        expect(n.get_number_name).to eq("zero")
+        end
+
+        it "return five" do 
+        n.current_number = 5
+        expect(n.get_number_name).to eq("five")
+        end 
+
+        it "returns thirteen" do 
+        n.current_number = 13
+        expect(n.get_number_name).to eq("thirteen")
+        end
+
+        it "returns fifteen" do 
+            n.current_number = 15
+            expect(n.get_number_name).to eq("fifteen")
+        end 
+
+        it "returns twenty" do
+            n.current_number = 20 
+            expect(n.get_number_name).to eq("twenty") 
+        end
+
+        it "returns hundred" do 
+            n.current_number = 100
+            expect(n.get_number_name).to eq("hundred")
+        end
+
+        it "returns hundred" do 
+            n.current_number = 1000
+            expect(n.get_number_name).to eq("thousand")
+        end
     end
 
-    it "return five" do 
-      n.current_number = 5
-      expect(n.get_number_name).to eq("five")
-    end 
+    describe "unknown numbers" do
+        it "returns one thousand, one hundred and twenty eigth" do 
+            n.current_number = 1128
+            expect(n.get_number_name).to eq("one thousand, one hundred and twenty eigth")
+        end
 
-    it "returns thirteen" do 
-      n.current_number = 13
-      expect(n.get_number_name).to eq("thirteen")
-    end
+        it "returns one thousand, nine hundred and ninety five" do 
+            n.current_number = 1995
+            expect(n.get_number_name).to eq("one thousand, nine hundred and ninety five")
+        end
 
-    it "returns fifteen" do 
-        n.current_number = 15
-        expect(n.get_number_name).to eq("fifteen")
-    end 
-
-    it "returns twenty" do
-        n.current_number = 20 
-        expect(n.get_number_name).to eq("twenty") 
-    end
-
-    it "returns hundred" do 
-        n.current_number = 100
-        expect(n.get_number_name).to eq("hundred")
-    end
-
-    it "returns hundred" do 
-        n.current_number = 1000
-        expect(n.get_number_name).to eq("thousand")
+        it "returns Invalid number" do 
+            n.current_number = 2000
+            expect(n.get_number_name).to eq("Invalid number")
+        end
     end
 
 end
